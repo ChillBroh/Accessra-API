b593cf7834c2d783d4df342f8c0d7006b6a551a8
Chillbroh <isharamadusanka410@gmail.com>
2025-04-13 18:59:24 +0530

    exlint fixed

--- src/migrations/public/1744545894744-add-tenant.ts
+++ src/migrations/public/1744545894744-add-tenant.ts
@@ -1,14 +1,14 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddTenant1744545894744 implements MigrationInterface {
-    name = 'AddTenant1744545894744';
-    
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        // Enable uuid-ossp extension if not already enabled
-        await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
-        
-        await queryRunner.query(
-            `CREATE TABLE "tenant" (
+  name = 'AddTenant1744545894744';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // Enable uuid-ossp extension if not already enabled
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+
+    await queryRunner.query(
+      `CREATE TABLE "tenant" (
                 "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
                 "created_at" TIMESTAMP NOT NULL DEFAULT now(),
                 "updated_at" TIMESTAMP NOT NULL DEFAULT now(),
@@ -17,11 +17,11 @@ export class AddTenant1744545894744 implements MigrationInterface {
                 CONSTRAINT "UQ_tenant_name" UNIQUE ("name"),
                 CONSTRAINT "UQ_tenant_schema_name" UNIQUE ("schema_name"),
                 CONSTRAINT "PK_tenant" PRIMARY KEY ("id")
-            )`
-        );
-    }
+            )`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query('DROP TABLE "tenant"');
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('DROP TABLE "tenant"');
+  }
 }
